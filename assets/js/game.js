96facd4b77b362121f011f4c822111af45c29863
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-08-11 13:21:27 -0500

    added playerName var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// this will do math and log 20
+console.log(10+10);
+console.log("Our robots name is " + playerName);
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
